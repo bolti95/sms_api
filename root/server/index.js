365e4db82b1639c312b6ea59c8f94d25a3d6f699
izzybolt <izzybolt95@gmail.com>
2021-03-15 14:32:04 +0000

    updated config

--- root/server/index.js
+++ root/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 3006;
-require('dotenv').config;
+require('dotenv').config();
 
 // init app
 const app = express();
